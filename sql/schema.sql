72edf27fecb996fe468e97c01ff8bb7b249b2661
baimuhtar <baimuhtar1997@gmail.com>
2023-02-14 07:49:26 +0600

    Added schema

---
+++ sql/schema.sql
@@ -0,0 +1,36 @@
+create table categories
+(
+    id serial8,
+    name varchar not null,
+    primary key (id)
+);
+
+create table options
+(
+    id serial8,
+    category_id int8    not null,
+    name        varchar not null,
+    primary key (id),
+    foreign key (category_id) references categories (id),
+    unique (category_id, name)
+);
+
+create table products (
+    id serial8,
+    category_id int8 not null,
+    name varchar not null,
+    price int4 not null,
+    primary key (id),
+    foreign key (category_id) references categories(id),
+    unique (name)
+);
+create table values (
+    id serial8,
+    product_id int8 not null,
+    option_id int8 not null,
+    value varchar not null,
+    primary key (id),
+    foreign key (product_id) references products (id),
+    foreign key (option_id) references options (id),
+    unique (product_id, option_id)
+);
